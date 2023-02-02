304254936a374eb2da44e32eb5850906a2bac60d
Jan Hellmich <jan@opal.dev>
2023-02-01 21:46:29 -0500

    Fix up tests for group reviewers

---
+++ opal/test_utils.go
@@ -0,0 +1,22 @@
+package opal
+
+import "fmt"
+
+func testReviewerStage(op string, requireManagerApproval bool, reviewerIDs ...string) string {
+	reviewerString := ""
+	for _, reviewerID := range reviewerIDs {
+		reviewerString += fmt.Sprintf(`
+reviewer {
+	id = "%s"
+}
+`, reviewerID)
+	}
+
+	return fmt.Sprintf(`
+reviewer_stage {
+	operator = "%s"
+	require_manager_approval = "%t"
+	
+	%s
+}`, op, requireManagerApproval, reviewerString)
+}
